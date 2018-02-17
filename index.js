88f4ccd65675508d3e53d2786d5f65c648b278f2
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-02-17 11:56:37 +0700

    Refactor Arrow Function

--- index.js
+++ index.js
@@ -14,52 +14,36 @@ const MongoClient = MongoDB.Client
 
 const mongoURL = 'mongodb://localhost:27017/arxtest1'
 
-app.post('/admin/createdb', function(req, res){
-  MongoClient.connect(mongoURL, function(err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
-  });
+app.post('/admin/createdb', (req, res) => MongoClient.connect(mongoURL, (err, db) => {
+    if (err) throw err
+    console.log("Database created!")
+    db.close()
+  })
+)
+
+io.on('connection', socket => {
+  console.log('a user conected')
+  socket.on('send-message', msg => {
+    console.log('a user send: ', msg)
+    io.emit('forward-message', msg)
+  })
+
+  socket.on('MESH_DELIVERY', dict => {
+    console.log('mesh triangles:', dict.triangles)
+    console.log('mesh uv:', dict.uv)
+    console.log('mesh vertices:', dict.vertices)
+    io.emit('INCOMING_MESH', dict)
+  })
+
+  socket.on('TAG_DELIVERY', tag => {
+    console.log(tag)
+  })
 })
-
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-//   socket.on('disconnect', function(){
-//     console.log('user disconnected');
-//   });
-// });
-
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     console.log('message: ' + msg);
-//   });
-// });
-
-io.on('connection', function(socket){
-  console.log('a user conected');
-  socket.on('send-message', function(msg){
-    console.log('a user send: ', msg);
-    io.emit('forward-message', msg);
-  });
-
-  socket.on('MESH_DELIVERY', function(dict){
-    console.log('mesh triangles:', dict.triangles);
-    console.log('mesh uv:', dict.uv);
-    console.log('mesh vertices:', dict.vertices);
-    io.emit('INCOMING_MESH', dict);
-  });
-
-  socket.on('TAG_DELIVERY', function(tag){
-    console.log(tag);
-  });
-});
-
-// io.emit('some event', { for: 'everyone' });
 
 function meshToString(mesh) {
 
 }
 
 server.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+  console.log('listening on *:3000')
+})
