220a0646f346784677907f34ef9d1408933dc926
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-08 17:47:39 +0700

    Create method

--- src/modules/mongo/index.js
+++ src/modules/mongo/index.js
@@ -2,9 +2,19 @@ import { DATABASE_NAME, MONGO_URI } from 'modules/mongo/constants'
 
 import { Client } from 'mongodb'
 
-function connect(cb) {
+function connect(collection, cb) {
     Client.connect(URI, (err, db) => {
         if (err) cb(err)
-        else cb(null, db.db(DATABASE_NAME))
+        else cb(null, db.db(DATABASE_NAME).collection(collection))
+    })
+}
+
+export function insert(collection, data, cb) {
+    connect(collection, (err, collection) => {
+        if (err) cb(err)
+        else collection.insertOne(data, (err, res) => {
+            if (err) cb(err)
+            else cb(null, res)
+        })
     })
 }
