2e16383deb6c184fc79e9365014f8d97a854760d
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-09 11:35:36 +0700

    Move Error

--- src/modules/error/index.js
+++ src/modules/error/index.js
@@ -1,4 +1,10 @@
 
+export const errors = {
+    UNAUTHORIZED: 'Unauthorized',
+    TOKEN_LOST: 'Token Lost',
+    INTERNAL_ERROR: 'Internal Error'
+}
+
 export function throwError (error) {
     return { error }
 } 
