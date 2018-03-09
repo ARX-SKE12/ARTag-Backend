6efcd15cb07155bc6fe8a3fa1b6bde30cfdefc31
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-09 11:36:26 +0700

    Change location to save error

--- src/modules/auth/constants.js
+++ src/modules/auth/constants.js
@@ -4,9 +4,3 @@ export const events = {
     AUTH_ERROR: 'auth-error',
     AUTH_SUCCESS: 'auth-success'
 }
-
-export const errors = {
-    UNAUTHORIZED: 'Unauthorized',
-    TOKEN_LOST: 'Token Lost',
-    INTERNAL_ERROR: 'Internal Error'
-}
--- src/modules/mongo/constants.js
+++ src/modules/mongo/constants.js
@@ -3,7 +3,3 @@ import DotEnv from 'dotenv'
 DotEnv.config()
 
 export const MONGO_URI = process.env.MONGO_URI
-
-export const errors = {
-    MONGO_ERROR: 'Internal MongoDB Error'
-}
