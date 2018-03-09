51f9d47182d54442da36baf9afc698ffe535f3dd
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-09 15:45:30 +0700

    Create Place

--- src/modules/place/actions/createPlace.js
+++ src/modules/place/actions/createPlace.js
@@ -1,21 +1,28 @@
 import { errors, throwError } from 'modules/error'
+import { resolveUser, resolveUserId } from 'modules/user'
 
 import { PLACE_COLLECTION } from 'modules/place/constants'
 import { create } from 'modules/mongo'
 import events from 'modules/place/events'
-import { resolveUserId } from 'modules/user'
 
-function createPlace(socket, placeData) {
+function resolvePlaceUser(socket, token, placeData) {
+    resolveUser(token, placeData, (err, resolvedUserData) => {
+        if (err) throwError(socket, events.PLACE_ERROR, errors.INTERNAL_ERROR)
+        else socket.emit(events.PLACE_CREATE, resolvedUserData)
+    })
+}
+
+function createPlace(socket, token, placeData) {
     create(PLACE_COLLECTION, placeData, (err, place) => {
         if (err) throwError(socket, events.PLACE_ERROR, errors.INTERNAL_ERROR)
-        else socket.emit(events.PLACE_CREATE, place)
+        else resolvePlaceUser(socket, token, place)
     })
 }
 
 function resolvePlaceCreator(socket, token, placeData) {
     resolveUserId(token, placeData, (err, resolvedUserIdPlaceData) => {
       if (err) throwError(socket, events.PLACE_ERROR, errors.INTERNAL_ERROR)
-      else createPlace(socket, resolvedUserIdPlaceData)
+      else createPlace(socket, token, resolvedUserIdPlaceData)
     })
 }
 
--- src/modules/user/index.js
+++ src/modules/user/index.js
@@ -1,7 +1,7 @@
 import { getMe, getUser } from 'modules/facebook'
 
 export function resolveUser(accessToken, data, cb) {
-    getUser(token, data.user, (err, userData) => {
+    getUser(accessToken, data.user, (err, userData) => {
         if (err) cb(err)
         else {
             data.user = userData
