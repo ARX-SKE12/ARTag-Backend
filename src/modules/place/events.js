08f64ac5f455c678882c98788343bf3d5183051d
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-11 19:42:05 +0700

    Remove Unused

--- src/modules/place/events.js
+++ src/modules/place/events.js
@@ -4,9 +4,7 @@ export default {
     PLACE_CREATE_SUCCESS: 'place-create-success',
     PLACE_CREATE_ERROR: 'place-create-error',
     PLACE_DATA_UPDATE: 'place-data-update',
-    PLACE_RETRIEVE: 'place-retrieve',
     PLACE_LIST: 'place-list',
     PLACE_LIST_REQUEST: 'place-list-request',
-    PLACE_LIST_ERROR: 'place-list-error',
-    PLACE_ERROR: 'place-error'
+    PLACE_LIST_ERROR: 'place-list-error'
 }
--- src/modules/place/index.js
+++ src/modules/place/index.js
@@ -1,10 +1,8 @@
 import createPlace from 'modules/place/actions/createPlace'
 import events from 'modules/place/events'
 import listPlace from 'modules/place/actions/listPlace'
-import retrievePlace from 'modules/place/actions/retrievePlace'
 
 export default (io, socket) => {
     socket.on(events.PLACE_CREATE, data => createPlace(io, socket, data))
     socket.on(events.PLACE_LIST_REQUEST, () => listPlace(socket))
-    socket.on(events.PLACE_RETRIEVE, data => retrievePlace(socket, data))
 }
