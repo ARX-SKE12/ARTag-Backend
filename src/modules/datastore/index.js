50f49b5b09fdfafb4705c7021437950913ea642c
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-13 16:06:05 +0700

    Refactor

--- src/modules/datastore/index.js
+++ src/modules/datastore/index.js
@@ -4,30 +4,24 @@ import PROJECT_ID from 'modules/datastore/constants'
 const datastore = Datastore({ projectId: PROJECT_ID })
 
 export function create(kind, data) {
-    const key = datastore.key([kind])
-    const entity = {
-        key,
-        data
-    }
+    const key = datastore.key([ kind ])
+    const entity = { key, data }
     return datastore.save(entity)
 }
 
 export function update(kind, id, data) {
-    const key = datastore.key([kind, datastore.int(id)])
-    const entity = {
-        key,
-        data
-    }
+    const key = datastore.key([ kind, datastore.int(id) ])
+    const entity = { key, data }
     return datastore.save(entity)
 }
 
 export function retrieve(kind, id) {
-    const key = datastore.key([kind, datastore.int(id)])
+    const key = datastore.key([ kind, datastore.int(id) ])
     return datastore.get(key)
 }
 
 export function list(kind) {
-    const key = datastore.key([kind])
+    const key = datastore.key([ kind ])
     const query = datastore.createQuery(key)
     return datastore.runQuery(query)
 }
