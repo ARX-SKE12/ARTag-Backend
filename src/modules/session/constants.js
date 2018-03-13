323a04d901970dea899a95b795064a4237ed9e35
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-14 00:34:56 +0700

    GCloud Datastore Session

--- src/modules/session/constants.js
+++ src/modules/session/constants.js
@@ -4,4 +4,8 @@ DotEnv.config()
 
 export const SESSION_SECRET_KEY = process.env.SESSION_SECRET_KEY
 
-export const MONGO_URI = process.env.MONGO_URI
+export const SESSION_PREFIX = 'express-session'
+
+export const GC_PROJECT_ID = process.env.GCLOUD_PROJECT
+
+export const GC_APP_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS
