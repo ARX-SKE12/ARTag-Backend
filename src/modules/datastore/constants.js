0898521b9b078e87e3eb965163f28adf089bbf02
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-13 15:35:13 +0700

    Datastore method

---
+++ src/modules/datastore/constants.js
@@ -0,0 +1,2 @@
+
+export const PROJECT_ID = 'arx-ske'
---
+++ src/modules/datastore/index.js
@@ -0,0 +1,33 @@
+import Datastore from '@google-cloud/datastore'
+import PROJECT_ID from 'modules/datastore/constants'
+
+const datastore = Datastore({ projectId: PROJECT_ID })
+
+export function create(kind, data) {
+    const key = datastore.key([kind])
+    const entity = {
+        key,
+        data
+    }
+    return datastore.save(entity)
+}
+
+export function update(kind, id, data) {
+    const key = datastore.key([kind, datastore.int(id)])
+    const entity = {
+        key,
+        data
+    }
+    return datastore.save(entity)
+}
+
+export function retrieve(kind, id) {
+    const key = datastore.key([kind, datastore.int(id)])
+    return datastore.get(key)
+}
+
+export function list(kind) {
+    const key = datastore.key([kind])
+    const query = datastore.createQuery(key)
+    return datastore.runQuery(query)
+}
