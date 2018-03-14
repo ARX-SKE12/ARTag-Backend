54379aa266c053bb9e8389fbc5aad7ef0f91f135
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-14 22:06:05 +0700

    Tag Event

--- src/modules/tag/events.js
+++ src/modules/tag/events.js
@@ -1,4 +1,9 @@
 
 export default {
-
+    TAG_CREATE: 'tag-create',
+    TAG_CREATE_SUCCESS: 'tag-create-success',
+    TAG_EDIT: 'tag-edit',
+    TAG_EDIT_SUCCESS: 'tag-edit-success',
+    TAG_UPDATE: 'tag-update',
+    TAG_ERROR: 'tag-error'
 }
