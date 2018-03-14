0f4d1b5ccbc335a6f286009793eb09c89761c724
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-14 22:25:07 +0700

    Init Remove Tag Event

--- src/modules/tag/events.js
+++ src/modules/tag/events.js
@@ -4,6 +4,8 @@ export default {
     TAG_CREATE_SUCCESS: 'tag-create-success',
     TAG_EDIT: 'tag-edit',
     TAG_EDIT_SUCCESS: 'tag-edit-success',
+    TAG_REMOVE: 'tag-remove',
+    TAG_REMOVE_SUCCESS: 'tag-remove-success',
     TAG_UPDATE: 'tag-update',
     TAG_ERROR: 'tag-error'
 }
--- src/modules/tag/index.js
+++ src/modules/tag/index.js
@@ -1,8 +1,10 @@
 import createTag from 'modules/tag/actions/createTag'
 import editTag from 'modules/tag/actions/editTag'
 import events from 'modules/tag/events'
+import removeTag from 'modules/tag/actions/removeTag'
 
 export default (io, socket) => {
     socket.on(events.TAG_CREATE, data => createTag(io, socket, data))
     socket.on(events.TAG_EDIT, data => editTag(io, socket, data))
+    socket.on(events.TAG_REMOVE, data => removeTag(io, socket, data))
 }
