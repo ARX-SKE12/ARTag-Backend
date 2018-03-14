9c6a8945efe9e92f560934ab3dce10401bebbff6
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-14 11:35:32 +0700

    Init Room event

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import DotEnv from 'dotenv'
 import Express from 'express'
 import Http from 'http'
 import PlaceEvent from 'modules/place'
+import RoomEvent from 'modules/room'
 import Session from 'modules/session'
 import Socket from 'socket.io'
 
@@ -11,13 +12,19 @@ DotEnv.config()
 
 const app = Express()
 const server = Http.Server(app)
+
+const PORT = process.env.BACKEND_PORT
+
+server.listen(PORT, err => {
+  if (err) console.error(err)
+  else console.log(`ARTag Backend service is listening on *:${PORT}`)
+})
+
 const io = Socket(server)
 
 BodyParser(app)
 
 Session(app, io)
-
-const PORT = process.env.BACKEND_PORT
 
 io.on('connection', socket => {
   
@@ -29,9 +36,6 @@ io.on('connection', socket => {
 
   PlaceEvent(io, socket)
 
-})
+  RoomEvent(io, socket)
 
-server.listen(PORT, err => {
-  if (err) console.error(err)
-  else console.log(`ARTag Backend service is listening on *:${PORT}`)
 })
