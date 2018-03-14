0004295085d4ca38defe3d31947307b6f3f9cec4
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-14 22:12:54 +0700

    Initialize Tag Module Structure

--- src/modules/tag/index.js
+++ src/modules/tag/index.js
@@ -1,5 +1,8 @@
+import createTag from 'modules/tag/actions/createTag'
+import editTag from 'modules/tag/actions/editTag'
 import events from 'modules/tag/events'
 
 export default (io, socket) => {
-    
+    socket.on(events.TAG_CREATE, data => createTag(io, socket, data))
+    socket.on(events.TAG_EDIT, data => editTag(io, socket, data))
 }
