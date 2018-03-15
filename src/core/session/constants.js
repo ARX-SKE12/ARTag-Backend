07a6f0e675ad42b0735b011930ac6415b05af2e2
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-15 17:11:44 +0700

    Redis Session

--- src/core/session/constants.js
+++ src/core/session/constants.js
@@ -4,8 +4,6 @@ DotEnv.config()
 
 export const SESSION_SECRET_KEY = process.env.SESSION_SECRET_KEY
 
-export const SESSION_PREFIX = 'express-session'
+export const REDIS_HOST = process.env.REDIS_HOST
 
-export const GC_PROJECT_ID = process.env.GCLOUD_PROJECT
-
-export const GC_APP_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS
+export const REDIS_PORT = process.env.REDIS_PORT
