a66de957474c50d63ea4622715643654556d57ed
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-25 08:45:26 +0700

    Plane Event

--- src/modules/plane/actions/planeUpdate.js
+++ src/modules/plane/actions/planeUpdate.js
@@ -1,4 +1,25 @@
+import { errors, throwError } from 'utils/error'
+import { retrieve, update } from 'utils/datastore'
 
-export async function planeUpdate(socket, planeData) {
-    
+import { PLACE_KIND } from 'modules/plane/constants'
+import to from 'await-to-js'
+
+export async function planeUpdate(socket, planeData, io) {
+    const { user } = socket.handshake.session
+    const { id, data } = planeData
+    const [ retrieveErr, place ] = await to(retrieve(PLACE_KIND, id))
+    if (retrieveErr) throwError(socket, events.PLANE_ERROR, errors.INTERNAL_ERROR)
+    else {
+        if (place.user !== user) throwError(socket, events.PLANE_ERROR, errors.PERMISSION_DENIED)
+        else {
+            const updateData = {
+                planes: data
+            }
+            const [ updateErr ] = await to(update(PLACE_KIND, id, updateData))
+            if (updateErr) throwError(socket, events.PLANE_ERROR, errors.INTERNAL_ERROR)
+            else {
+                io.to(id).emit(events.PLANE_UPGRADE)
+            }
+        }
+    }
 }
--- src/modules/plane/events.js
+++ src/modules/plane/events.js
@@ -1,4 +1,6 @@
 
 export default {
-    PLANE_UPDATE: 'plane-update'
+    PLANE_UPDATE: 'plane-update',
+    PLANE_UPGRADE: 'plane-upgrade',
+    PLANE_ERROR: 'plane-error'
 }
