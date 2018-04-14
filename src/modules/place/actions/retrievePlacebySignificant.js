fb26c2c70b348ceb10f8173a3343b5b97a913154
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-14 10:52:28 +0700

    retrive by significant resolve user object

--- src/modules/place/actions/retrievePlacebySignificant.js
+++ src/modules/place/actions/retrievePlacebySignificant.js
@@ -4,6 +4,7 @@ import { Base64 } from 'js-base64'
 import { PLACE_KIND } from 'modules/place/constants'
 import events from 'modules/place/events'
 import { query } from 'utils/datastore'
+import { resolveUserObject } from 'utils/user'
 import to from 'await-to-js'
 
 const TIMESTAMP_FIELD = 'timestamp'
@@ -26,5 +27,16 @@ export default async function  retrievePlacebySignificant(socket, data) {
     }]
     const [ queryErr, place ] = await to(query(PLACE_KIND, { filters }))
     if (queryErr) throwError(socket, events.PLACE_ERROR_SIGNIFICANT, errors.INTERNAL_ERROR)
-    else socket.emit(events.PLACE_RESPONSE_SIGNIFICANT, place[0])
+    else {
+        if (token) {
+            const [ resolveErr, placeUser ] = resolveUserObject(token, place[0])
+            if (!place[0].is_public && (place[0].user !== user || place[0].users.indexOf(user) <0)) throwError(socket, events.PLACE_ERROR_SIGNIFICANT, errors.UNAUTHORIZED)
+            else {
+                if (resolveErr) throwError(socket, events.PLACE_ERROR_SIGNIFICANT, errors.INTERNAL_ERROR)
+                else {
+                    socket.emit(events.PLACE_RESPONSE_SIGNIFICANT, placeUser)
+                }
+            }
+        } else throwError(socket, events.PLACE_ERROR_SIGNIFICANT, errors.TOKEN_LOST)
+    }
 }
